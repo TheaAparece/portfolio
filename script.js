e885c168c04437ea441bc8ce17bd41af4ef196b0
Meepcoder12 <168708135+Meepcoder12@users.noreply.github.com>
2026-07-30 22:13:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", () => {
+
+    const nav = document.querySelector("nav");
+
+    if(window.scrollY > 50){
+        nav.style.background="rgba(0,0,0,0.8)";
+    }
+    else{
+        nav.style.background="rgba(0,0,0,0.5)";
+    }
+
+});
